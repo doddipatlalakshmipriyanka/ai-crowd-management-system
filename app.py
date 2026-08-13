08406121341e5d7c3637c284df8853e77a0a9723
Lakshmi Priyanka Doddipatla <doddipatlapriyanka1@gmail.com>
2026-08-13 14:10:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # =========================================================
 
 st.set_page_config(
-    page_title="AI Crowd Management System",
+    page_title="CROWDGURD AI-Intelligent Crowd Management System",
     page_icon="👥",
     layout="wide"
 )
